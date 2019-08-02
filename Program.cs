ea8dacf5ec26ed2fef0234c1783e115a33d4e8e7
brunoborbadev <brunob_dev@outlook.com>
2019-08-02 19:21:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace VectorExc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Product[] vect = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Name = nome, Price = preco };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
